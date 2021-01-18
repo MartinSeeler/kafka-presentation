624901102982d9f3f21224bc060378ff37a41b49
Luis Alfredo Godoy Alvarez <luisalfredo.godoyalvarez@paessler.com>
2021-01-18 09:46:09 +0100

    Reformat files with warnings

--- consumer.py
+++ consumer.py
@@ -1,13 +1,15 @@
 from kafka import KafkaConsumer
 import json
 
-consumer = KafkaConsumer('names', 
-	group_id='c1', 
-	auto_offset_reset='earliest',
-	bootstrap_servers=['192.168.178.26:9092'],
-	value_deserializer=lambda m: json.loads(m.decode('utf-8'))["value"])
+consumer = KafkaConsumer('names',
+                         group_id='c1',
+                         auto_offset_reset='earliest',
+                         bootstrap_servers=['192.168.178.26:9092'],
+                         value_deserializer=lambda m: json.loads(m.decode('utf-8'))["value"])
 
 for message in consumer:
-    print("%s:%d:%d: key=%s - %s" % (message.topic, message.partition,
-                                          message.offset, message.key,
-                                          message.value))
+    print("topic=%s "
+          "partition=%d "
+          "offset=%d "
+          "key=%s "
+          "value=%s" % (message.topic, message.partition, message.offset, message.key, message.value))
--- producer.py
+++ producer.py
@@ -7,21 +7,21 @@ import random
 names = []
 with open('names.txt', 'r') as f:
     for line in f:
-    	names.append(str(line.strip()))
+        names.append(str(line.strip()))
 random.shuffle(names)
 
 # connect to kafka
 producer = KafkaProducer(
-	bootstrap_servers=['localhost:9092'], 
-	value_serializer=lambda m: json.dumps({"value": m}).encode('utf-8')
+    bootstrap_servers=['localhost:9092'],
+    value_serializer=lambda m: json.dumps({"value": m}).encode('utf-8')
 )
 
 # send messages
 for name in names:
-	print(f"Sending {name}")
-	producer.send('names', 
-		#key=bytes(name[0], "utf-8"), 
-		#key=bytes(str(len(name)), "utf-8"), 
-		partition=0 if name[0]<="m" else 1,
-		value=name)
-	producer.flush()
+    print(f"Sending {name}")
+    producer.send('names',
+                  # key=bytes(name[0], "utf-8"),
+                  # key=bytes(str(len(name)), "utf-8"),
+                  partition=0 if name[0] <= "m" else 1,
+                  value=name)
+    producer.flush()
